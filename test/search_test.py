c27359d00afde733b1d280d814c0262f0dcd3026
olzeug <34414119+olzeug@users.noreply.github.com>
2026-04-27 12:37:10 +0000

    refactor: replace global test objects with pytest fixtures in search and recipe test suites

--- test/search_test.py
+++ test/search_test.py
@@ -1,37 +1,39 @@
 import pytest
 from get_chefkoch import Search, Recipe, exceptions
 
-s = Search()
+@pytest.fixture(scope="module")
+def s():
+    return Search()
 
 
-def test__repr__type():
+def test__repr__type(s):
     assert isinstance(repr(s), str)
     
-def test_argsToUrlParams():
+def test_argsToUrlParams(s):
     assert s._argsToUrlParams(t="23", e={}, f=[], i=1, b="test") == 't=23&e={}&f=[]&i=1&b=test'
     
-def test_invalid_search_and_suggestions():
+def test_invalid_search_and_suggestions(s):
     with pytest.raises(AttributeError) as e_info:
         s.recipes()
     
     with pytest.raises(AttributeError) as e_info:
         s.suggestions()
 
-def test_search_recipes():
+def test_search_recipes(s):
     recipes = s.recipes("Apfelstrudel")
     assert isinstance(recipes, list)
     for recipe in recipes:
         assert isinstance(recipe, Recipe)
         
-def test_invalid_limit_offset_type():
+def test_invalid_limit_offset_type(s):
     with pytest.raises(TypeError) as e_info:
         s.recipes(limit="0")
     with pytest.raises(TypeError) as e_info:
         s.recipes(offset="0")    
     
-def test_suggestions():
+def test_suggestions(s):
     assert isinstance(s.suggestions("Toast"), dict)
     
-def test_recipeOfTheDay():
+def test_recipeOfTheDay(s):
     assert isinstance(s.recipeOfTheDay(), Recipe)
     
